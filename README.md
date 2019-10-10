1ba801262401c40f271fa0e2ffbc524823fe7868
Riley Brenner <rbrenner1124@gmail.com>
2019-10-10 14:36:32 -0700

    js, read me, html updates

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # psychic_game
+
+Psychic - Game
+
+The goal of the game is for the user to pick a letter that the computer randoms picks.
+
+This game was created with HTML, Javascript and Bootstrap. 
---
+++ assets/javascript/game.js
@@ -0,0 +1,63 @@
+//list of all options for game guesses 
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+//track game stats 
+var remainingGuesses = 9;
+var wins = 0;
+var losses = 0;
+var computerGuess = [];
+var lettersGuessed = [];
+
+//user choice
+document.onkeyup = function(event) {
+	var userGuess = event.key;
+	lettersGuessed.push(userGuess);
+	console.log(computerGuess[0]);
+
+//computer choice
+window.onload = function() {
+	var compGuess = options[Math.floor(Math.random() * options.length)];
+	computerGuess.push(compGuess);
+	console.log(computerGuess[0]);
+}
+// if the player chooses the same letter as computer, you win!
+
+if ((userGuess === computerGuess[0]) && (remainingGuesses > 0)) {
+	wins++;
+	remainingGuesses = 9;
+	lettersGuessed.length = 0;
+	computerGuess.length = 0;
+	var compGuess = options[Math.floor(Math.random() * options.length)];
+	computerGuess.push(compGuess);
+	console.log(computerGuess[0]);
+}
+
+// if the player does not the same letter as computer, you lose!
+
+
+else if ((userGuess !== computerGuess[0]) && (remainingGuesses > 0)) {
+	remainingGuesses = remainingGuesses-1;
+}
+
+else {
+	losses++;
+	remainingGuesses = 9;
+	lettersGuessed.length = 0;
+	computerGuess.length = 0;
+	var compGuess = options[Math.floor(Math.random() * options.length)];
+	computerGuess.push(compGuess);
+	console.log(computerGuess[0]);
+}
+
+// Math to render proper html
+
+var html = "<p>Guess what letter I'm thinking of!</p>" +
+          "<p>Wins: " + wins + "</p>" +
+          "<p>Losses: " + losses + "</p>" +
+          "<p>Guesses left: " + remainingGuesses + "</p>" +
+          "<p>Your guesses so far: " + lettersGuessed + "</p>";
+ 
+document.querySelector("#game").innerHTML = html;
+
+}
